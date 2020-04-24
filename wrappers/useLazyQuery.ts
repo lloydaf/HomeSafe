a111d4fb99324359a2426667204eb65ad136c604
Lloyd Francis <lloydaf@gmail.com>
2020-04-25 05:01:20 +0530

    Changing imperative query to a wrapper of useLazyQuery

---
+++ wrappers/useLazyQuery.ts
@@ -0,0 +1,14 @@
+import { useApolloClient, OperationVariables, DocumentNode } from "@apollo/client";
+import React from 'react';
+
+export function useLazyQuery<TData = any, TVariables = OperationVariables>(query: DocumentNode) {
+  const client = useApolloClient();
+  return React.useCallback(
+    (variables: TVariables) =>
+      client.query<TData, TVariables>({
+        query: query,
+        variables: variables,
+      }),
+    [client]
+  );
+}
