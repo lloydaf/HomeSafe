903331590beb44daf4394790d83cf25c172adff4
Lloyd Francis <lloydaf@gmail.com>
2020-04-29 03:56:53 +0530

    Registering expo token when user registers

--- routes/login/Login.tsx
+++ routes/login/Login.tsx
@@ -2,12 +2,13 @@ import { TextInput, Button, AsyncStorage } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { Subject } from 'rxjs';
 import { debounceTime, filter, tap } from 'rxjs/operators';
-import { GET_USER, REGISTER_USER } from 'graphql-schema/users';
+import { GET_USER, REGISTER_USER, SET_TOKEN } from 'graphql-schema/users';
 import { User } from 'models/users';
 import { useLazyQueryAsync } from 'wrappers';
 import { useMutation } from '@apollo/client';
 import { Config } from 'utils/expo/config.util';
-import { UserContext } from 'stores/users';
+import { UserContext, UserContextType } from 'stores/users';
+import { getToken } from 'utils/expo/expo.util';
 
 const username$: Subject<string> = new Subject();
 
@@ -22,17 +23,23 @@ export const Login = () => {
     username$.next(username);
   }
 
-  const [registerUser, { data, error }] = useMutation(REGISTER_USER.mutation)
+  const [action, { data: tokenData }] = useMutation(SET_TOKEN.mutation);
 
-  const registerAndNavigate = async (callback) => {
+  const [registerUser, { data: registrationData, error }] = useMutation(REGISTER_USER.mutation);
+
+  const registerAndNavigate = async ({ login }: UserContextType) => {
     try {
       await Promise.all([
         registerUser({
           variables: REGISTER_USER.variables({ username, fullName, phoneNumber })
         }),
-        AsyncStorage.setItem(Config.UserName, username)
+        AsyncStorage.setItem(Config.UserName, username),
       ]);
-      callback();
+      const expoToken = await getToken();
+      await action({
+        variables: SET_TOKEN.variables({ username, expoToken })
+      });
+      login();
     } catch (error) {
       console.log('error', error);
     }
@@ -68,12 +75,12 @@ export const Login = () => {
   return (
     <UserContext.Consumer>
       {
-        ({ login }: any) => (
+        ({ login }: UserContextType) => (
           <>
             <TextInput placeholder={'Full Name'} defaultValue={fullName} onChangeText={setFullName}></TextInput>
             <TextInput placeholder={'Username'} defaultValue={username} onChangeText={fetchUser}></TextInput>
             <TextInput placeholder={'Phone Number'} defaultValue={phoneNumber} onChangeText={setPhoneNumber}></TextInput>
-            <Button title='Submit' disabled={!username || !fullName || !phoneNumber || disabled} onPress={() => registerAndNavigate(login)}></Button>
+            <Button title='Submit' disabled={!username || !fullName || !phoneNumber || disabled} onPress={() => registerAndNavigate({ login })}></Button>
           </>
         )
       }
